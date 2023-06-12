d2ed12892f5c92c6218032189d68ea58668eb65a
origamiwang <2241375541@qq.com>
2023-06-12 16:23:58 +0800

    重构前端

--- README.md
+++ README.md
@@ -4,10 +4,17 @@
 
 ![架构设计图](static/my_gpt.jpg)
 
-## 实现历史记录
+## 日志
 
 1. 存入mysql的数据，以历史记录的方式可视化在前端，用户点击访问某一次的记录，可以继续那次的对话
 2. 点击历史记录后，从mysql拿到那一次之前的所有消息并缓存在redis
 3. sendBeacon() 为了避免chrome在onBeforeUnload事件中禁用fetch
 4. mysql查询到的数据是tuple，redis不能用tuple、list，选用string，使用eval将string转化成list、tuple
 5. TODO: 点击历史对话 -> 将mysql的数据缓存到redis。**最后同步缓存的时候，只插入新增加的对话**
+6. 重构前端
+
+## css属性积累
+
+1. `word-wrap: break-word`,很多字母连在一起会被浏览器误认为是一个单词，而不会换行，因此需要设置此属性
+2. `width: fit-content` 和 `maxWidth: 50%`实现div宽度的自适应
+3. `clear: both`实现让设置了`float`的div可以独占一行
--- static/script.js
+++ static/script.js
@@ -15,6 +15,15 @@ window.onbeforeunload = () => {
     }
 }
 
+chatText.oninput = () => {
+    if (chatText.value === "") {
+        submitBtn.value = "Input"
+        submitBtn.setAttribute("disabled", "true")
+    } else {
+        submitBtn.value = "Send"
+        submitBtn.removeAttribute("disabled")
+    }
+}
 
 chatText.addEventListener("keyup", (event) => {
     // 回车 触发点击事件
@@ -31,7 +40,7 @@ function modify_submit_style() {
 
 function recover_submit_style() {
     submitBtn.removeAttribute('disabled')
-    submitBtn.value = 'Submit'
+    submitBtn.value = 'Send'
 }
 
 function fetch_session_id() {
@@ -44,20 +53,66 @@ function fetch_session_id() {
         });
 }
 
+function setDivStyle(style) {
+    style.backgroundColor = "#36292F"
+    style.color = "#F3F9F1"
+    style.width = "fit-content"
+    style.maxWidth = "38%"
+    style.padding = "1vw"
+    style.margin = "2vh 3vw"
+    style.borderRadius = "10px"
+    style.wordBreak = "break-word"
+}
+
+function addQuestionDiv(text) {
+    let question_div = document.createElement('div')
+    let style = question_div.style
+    question_div.innerText = text
+    // 设置样式
+    setDivStyle(style)
+    style.float = "right"
+
+    chatInfo.appendChild(question_div)
+}
+
+function addClearDiv() {
+    let clear_div = document.createElement('div')
+    clear_div.style.clear = "both"
+    chatInfo.appendChild(clear_div)
+}
+
+function addAnswerDiv() {
+    let answer_div = document.createElement('div')
+    let style = answer_div.style
+    // 设置样式
+    setDivStyle(style)
+
+    chatInfo.appendChild(answer_div)
+    return answer_div
+}
+
+function appendAnswerText(question_div, text) {
+    question_div.innerHTML = question_div.innerHTML + text
+}
+
 function submit() {
     console.log("submit...")
     modify_submit_style()
     let question = chatText.value
-
-    console.log("input question is: " + question)
+    // 清空输入框
+    chatText.value = ""
+    addQuestionDiv(question)
+    addClearDiv()
     let url = "/gpt/" + session_id + "?question=" + question
     let source = new EventSource(url);
+    let question_div = addAnswerDiv()
     source.onmessage = function (event) {
         if (event.data === "[DONE]") {
             recover_submit_style()
             source.close();
         } else {
-            chatInfo.innerText = chatInfo.innerText + event.data
+            appendAnswerText(question_div, event.data)
+            // chatInfo.innerText = chatInfo.innerText + event.data
         }
     }
 
