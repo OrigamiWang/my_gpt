d7f1b157036e10a50b1f9086da5dfa887ab3d8eb
origamiwang <2241375541@qq.com>
2023-06-14 10:24:08 +0800

    整理后端结构

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, request, make_response, jsonify, Response
 import os
 import sys
-from py.chat import get_name_by_question, chat_stream, load_his
-from py.mysql_history import insert_content_table, insert_message_table, query_content_list, get_sessionId_by_msgId, \
-    update_time
-from py.redis_cache import exists_key, set_kv, get_v, update_v, flush_cache
+from py.chat import get_name_by_question, chat_stream, load_his, cache_persistent_fun, get_content_list_fun, chatgpt_fun
+from py.mysql_history import query_content_list, get_sessionId_by_msgId
+from py.redis_cache import exists_key, set_kv, get_v, update_v
 from py.util import get_session_id, tuple_to_list, list_to_dict
 import traceback
 
@@ -38,32 +37,7 @@ def chatgpt(sessionId=None):
         question = request.args.get("question", "")
         resp = make_response(render_template('chatpgt.html'))
         if question:
-            message_key = str(sessionId) + "_message"
-            content_key = str(sessionId) + "_content"
-            print(exists_key(content_key))
-            if not exists_key(content_key):
-                # 通过查看redis缓存中是否存在这个key，来判断是否是这个会话的第一次
-                # 会话记录存入数据库
-                role = 'system'
-                name = get_name_by_question(question)
-                message_dict = {'name': name, 'sessionId': sessionId}
-                # message缓存
-                set_kv(message_key, str(message_dict))
-                # 初始化content缓存
-                set_kv(content_key, str([]))
-            else:
-                if not exists_key(message_key):
-                    set_kv(message_key, '')
-                role = 'user'
-            content_dict = {"sessionId": sessionId, "role": role, "content": question}
-            # 获取redis缓存的content并将此次提问加入缓存
-            content_arr = list(eval(get_v(content_key)))
-            content_arr.append(content_dict)
-            # 更新缓存
-            update_v(content_key, str(content_arr))
-            # 删除一些无关数据(sessionId)
-            for content in content_arr:
-                content.pop('sessionId')
+            content_arr = chatgpt_fun(sessionId, question)
             return Response(chat_stream(content_arr, sessionId), mimetype="text/event-stream")
         return resp
     except Exception as e:
@@ -71,39 +45,16 @@ def chatgpt(sessionId=None):
         print(e)
 
 
-@app.route('/cache/<sessionId>', methods=["POST", "GET"])
-def cache_persistent(sessionId):
-    message_key = str(sessionId) + "_message"
-    content_key = str(sessionId) + "_content"
-    content_num_key = str(sessionId) + "_content_num"
-    if exists_key(message_key) and get_v(content_key) is not None:
-        # 将redis缓存持久化到mysql数据库
-        if get_v(content_num_key) is None:
-            message_tuple = eval(get_v(message_key))
-            message = dict(message_tuple)
-            content_arr = list(eval(get_v(content_key)))
-            insert_message_table(message)
-        else:
-            # 注意，如果是通过历史记录进行的对话，一部分对话已经保存到了mysql，只需保存新产生的content，不需要添加message以及历史的content
-            # 但是，需要更新message的时间
-            update_time(sessionId)
-            content_arr = list(eval(get_v(content_key)))[int(get_v(content_num_key)):]
-        insert_content_table(content_arr)
-        # 清除缓存
-        flush_cache(sessionId)
-
-    return "cache"
-
-
 @app.route('/content/<msg_id>', methods=["GET", "POST"])
 def get_content_list(msg_id):
-    content_list = tuple_to_list(query_content_list(msg_id))
-    session_id = get_sessionId_by_msgId(msg_id)[0][0]
-    # 将历史记录、以及历史记录的数量缓存在redis
-    content_dict = list_to_dict(content_list)
-    set_kv(session_id + "_content", str(content_dict))
-    set_kv(session_id + "_content_num", str(len(content_list)))
+    session_id, content_list = get_content_list_fun(msg_id)
     return jsonify(session_id=session_id, content_list=content_list)
+
+
+@app.route('/cache/<sessionId>', methods=["POST", "GET"])
+def cache_persistent(sessionId):
+    cache_persistent_fun(sessionId)
+    return "cache"
 
 
 if __name__ == '__main__':
--- py/chat.py
+++ py/chat.py
@@ -1,8 +1,9 @@
 import openai
 
-from py.mysql_history import query_history
-from py.redis_cache import get_v, update_v, load_history_cache, set_kv
-from py.util import read_yaml, process_history_datetime, tuple_to_list
+from py.mysql_history import query_history, insert_message_table, update_time, insert_content_table, query_content_list, \
+    get_sessionId_by_msgId
+from py.redis_cache import get_v, update_v, set_kv, exists_key, flush_cache
+from py.util import read_yaml, process_history_datetime, tuple_to_list, list_to_dict
 
 
 def get_name_by_question(question):
@@ -50,3 +51,64 @@ def chat_stream(messages, sessionId):
 
 def load_his():
     return process_history_datetime(tuple_to_list(query_history()))
+
+
+def cache_persistent_fun(sessionId):
+    message_key = str(sessionId) + "_message"
+    content_key = str(sessionId) + "_content"
+    content_num_key = str(sessionId) + "_content_num"
+    if exists_key(message_key) and get_v(content_key) is not None:
+        # 将redis缓存持久化到mysql数据库
+        if get_v(content_num_key) is None:
+            message_tuple = eval(get_v(message_key))
+            message = dict(message_tuple)
+            content_arr = list(eval(get_v(content_key)))
+            insert_message_table(message)
+        else:
+            # 注意，如果是通过历史记录进行的对话，一部分对话已经保存到了mysql，只需保存新产生的content，不需要添加message以及历史的content
+            # 但是，需要更新message的时间
+            update_time(sessionId)
+            content_arr = list(eval(get_v(content_key)))[int(get_v(content_num_key)):]
+        insert_content_table(content_arr)
+        # 清除缓存
+        flush_cache(sessionId)
+
+
+def get_content_list_fun(msg_id):
+    content_list = tuple_to_list(query_content_list(msg_id))
+    session_id = get_sessionId_by_msgId(msg_id)[0][0]
+    # 将历史记录、以及历史记录的数量缓存在redis
+    content_dict = list_to_dict(content_list)
+    set_kv(session_id + "_content", str(content_dict))
+    set_kv(session_id + "_content_num", str(len(content_list)))
+    return session_id, content_list
+
+
+def chatgpt_fun(sessionId,question):
+    message_key = str(sessionId) + "_message"
+    content_key = str(sessionId) + "_content"
+    print(exists_key(content_key))
+    if not exists_key(content_key):
+        # 通过查看redis缓存中是否存在这个key，来判断是否是这个会话的第一次
+        # 会话记录存入数据库
+        role = 'system'
+        name = get_name_by_question(question)
+        message_dict = {'name': name, 'sessionId': sessionId}
+        # message缓存
+        set_kv(message_key, str(message_dict))
+        # 初始化content缓存
+        set_kv(content_key, str([]))
+    else:
+        if not exists_key(message_key):
+            set_kv(message_key, '')
+        role = 'user'
+    content_dict = {"sessionId": sessionId, "role": role, "content": question}
+    # 获取redis缓存的content并将此次提问加入缓存
+    content_arr = list(eval(get_v(content_key)))
+    content_arr.append(content_dict)
+    # 更新缓存
+    update_v(content_key, str(content_arr))
+    # 删除一些无关数据(sessionId)
+    for content in content_arr:
+        content.pop('sessionId')
+    return content_arr
